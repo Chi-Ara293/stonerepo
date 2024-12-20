039540131cab81a79ffe783ae94fa1ae05f76ce3
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2024-12-20 07:05:20 -0500

    deleted Nav.js and edit Header

--- stone/src/Header.js
+++ stone/src/Header.js
@@ -7,9 +7,17 @@ const imgStyle ={
 
 const Header = () => {
     return(
-     <div className="header">
-        <h1 style={{color:'white'}} >Grasstone</h1>
-        <img src={cloaf} alt="Logo" style={imgStyle}/>
+     <div className="container">
+         <div className="header">
+             <h1 style={{color:'white'}} >Grasstone</h1>
+             <img src={cloaf} alt="Logo" style={imgStyle}/>
+         </div>
+         <ul className="nav">
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+         </ul>
      </div>
     )
 }
