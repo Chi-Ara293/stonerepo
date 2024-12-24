8f76658d77ea90ea75b385d870e82f58ad971db9
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2024-12-23 20:20:13 -0500

    created confirmedBooking component

---
+++ stone/src/ConfirmedBooking.js
@@ -0,0 +1,10 @@
+const Confirmation = () => {
+    return(
+    <div>
+      <h2>Confirmation page</h2>
+      <p>Your booking has been successfully made!</p>
+    </div>
+    )
+}
+
+export default Confirmation;
