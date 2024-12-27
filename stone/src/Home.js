e7a87e7bf01da5ac90f1e238f0719d7cf351504d
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2024-12-26 19:39:31 -0500

    small changes

--- stone/src/Home.js
+++ stone/src/Home.js
@@ -10,15 +10,15 @@ const Home = () => {
          <div className='home'>
              <div className='card'>
                 <img src={sofacat} alt='drawing' style={{width: '200px', height: 'auto'}}/>
-                <h4>Today's poem</h4>
+                <h4>Hello</h4>
              </div>
              <div className='card2'>
                 <img src={blackcat} alt='drawing' style={{width: '300px', height: 'auto'}}/>
-                <h4>What's new</h4>
+                <h4>Everyone</h4>
              </div>
              <div className='card3'>
                 <img src={ctray} alt='drawing' style={{width: '300px', height: 'auto'}}/>
-                <h4>What's new</h4>
+                <h4>what's up?</h4>
              </div>
          </div>
       </div>
