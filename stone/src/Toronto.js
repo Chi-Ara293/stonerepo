d6dfd0c73aba65f3208aa6f5b9f3f85238da958d
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-01-19 08:40:25 -0500

    added location_icon

--- stone/src/Toronto.js
+++ stone/src/Toronto.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import location_icon from './assets/location_icon.png'
 
 const Toronto = () => {
     const [time, setTime] = useState(new Date().toLocaleString('en-US', {
@@ -26,9 +27,18 @@ const Toronto = () => {
       return () => clearInterval(interval);
     }, []);
 
+  const locationStyle={
+    width: '40px',
+    height: '40px',
+    objectFit: 'contain'
+  }
+
   return (
     <div className='toronto-container'>
-      <h4 className='toronto-title'>Toronto, Canada Time</h4>
+      <div className='location'>
+        <img src={location_icon} alt='Location-icon' style={locationStyle}></img>
+        <h4 className='toronto-title'>Toronto, Canada Time</h4>
+      </div>
       <p className='toronto-time'>{time}</p>
     </div>
   );
