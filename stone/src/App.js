3ba1082c89d36c7242a2a135e9dead121ebc0f3a
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-01-19 08:52:41 -0500

    deleted book file

--- stone/src/App.js
+++ stone/src/App.js
@@ -5,31 +5,11 @@ import Clocks from './Clocks.js';
 import Nav from './Nav.js';
 import Home from './Home.js';
 import About from './About.js'
-import Book from './Book.js'
-import Confirmation from './BookingConfirmation.js';
 import Contact from './Contact.js';
 import Footer from './Footer.js'
-import { useReducer } from 'react';
 
-const timeReducer = (state, action) => {
- switch(action.type){
-   case 'UPDATE_TIMES':
-    return{
-     ...state,
-     availableTimes: action.payload
-    };
-   default:
-    return state;
-  }
-}
-
-const initialState = {
-  availableTimes:["9:00", "10:00", "14:00", "15:00", "17:00", "18:00"]
-}
 
 function App() {
-  const [state, dispatch] = useReducer(timeReducer, initialState)
-
   return (
     <BrowserRouter>
      <div className="App">
@@ -40,8 +20,6 @@ function App() {
            <Route path="/" element={<Home/>}/>
            <Route path="about" element={<About/>}/>
            <Route path="contact" element={<Contact/>}/>
-           <Route path="book" element={<Book availableTimes={state.availableTimes} updateTimes={dispatch}/>}/>
-           <Route path="booking-confirmation" element={<Confirmation/>}/>
         </Routes>
        <Footer className='footer'/>
      </div>
--- stone/src/Nav.js
+++ stone/src/Nav.js
@@ -10,9 +10,6 @@ const Nav = () => {
          <Link to="/about">About</Link>
        </li>
        <li>
-         <Link to="/book">Book</Link>
-       </li>
-       <li>
          <Link to="/contact">Contact</Link>
        </li>
     </ul>
