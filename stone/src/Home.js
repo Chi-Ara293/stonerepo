dacc8bb1e166eb9317374ff68f59cb15eb90fe67
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-01-19 19:46:11 -0500

    delete home.css

--- stone/src/Home.js
+++ stone/src/Home.js
@@ -1,26 +1,7 @@
-import ctray from './assets/ctray.png'
-import blackcat from './assets/blackcat.jpg'
-import sofacat from './assets/sofacat.png'
-import './Home.css'
-
 const Home = () => {
     return(
       <div>
          <h1>Welcome to My Homepage</h1>
-         <div className='home'>
-             <div className='card'>
-                <img src={sofacat} alt='drawing' style={{width: '200px', height: 'auto'}}/>
-                <h4>Hello</h4>
-             </div>
-             <div className='card'>
-                <img src={blackcat} alt='drawing' style={{width: '300px', height: 'auto'}}/>
-                <h4>Everyone</h4>
-             </div>
-             <div className='card'>
-                <img src={ctray} alt='drawing' style={{width: '300px', height: 'auto'}}/>
-                <h4>what's up?</h4>
-             </div>
-         </div>
       </div>
     )
 }
