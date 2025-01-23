1a4b25049da59fbe58a060da17aa5a2ff98d77fd
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-01-23 12:39:39 -0500

    delete contact.css and adjust contact file

--- stone/src/Contact.js
+++ stone/src/Contact.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import './Contact.css';
 
 const Contact = () => {
  const [name, setName] = useState('');
@@ -9,59 +8,56 @@ const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(name, email, phone);
+        e.target.reset();
        }
 
-    const labelStyle = {
-        display: "block",
-        marginBottom: "5px"
-    }
-
-    const inputStyle = {
-        maxWidth: "400px",
-        borderRadius: "4px",
-        border: "1px solid #ccc"
-    }
-
     return(
-        <div>
+        <div className="contact">
            <h2>Contact</h2>
-           <p>Please fill the form below.</p>
-           <form onSubmit={handleSubmit} style={{maxWidth: "400px", margin:"0 auto"}}>
+           <p>Please DM me via Instagram or fill out the form below.</p>
+           <form className='form' onSubmit={handleSubmit}>
              <div>
-               <label htmlFor="name" style={labelStyle}>Name:</label>
-               <input
+               <label className='label' htmlFor="name">Name:</label>
+               <input className="feild"
                  type="text"
                  minLength="3"
                  maxLength="10"
                  id="name"
                  autoComplete="on"
                  required
-                 onChange={(e) => setName(e.target.value)}
-                 style={inputStyle}/>
+                 onChange={(e) => setName(e.target.value)}/>
              </div>
              <div>
-                <label htmlFor="email" style={labelStyle}>Email address:</label>
-                <input
+                <label className='label' htmlFor="email">Email address:</label>
+                <input className="feild"
                  type="email"
                  id="email"
                  name="email"
                  autoComplete="on"
                  required
-                 onChange={(e) => setEmail(e.target.value)}
-                 style={inputStyle}/>
+                 onChange={(e) => setEmail(e.target.value)}/>
              </div>
              <div>
-                <label htmlFor="phone" style={labelStyle}>Phone number (option):</label>
-                <input
+                <label className="label" htmlFor="phone">Phone number (option):</label>
+                <input className="feild"
                  type="tel"
                  id="phone"
                  name="phone"
                  autoComplete="on"
-                 onChange={(e) => setPhone(e.target.value)}
-                 style={inputStyle}/>
+                 onChange={(e) => setPhone(e.target.value)}/>
              </div>
-
-             <button type="submit" className="signin"><b>Sign In</b></button>
+             <div>
+                <label className="label" htmlFor="message">Message:</label>
+                <textarea className="feild2"
+                 type="message"
+                 id="message"
+                 name="message"
+                 rows='8'
+                 cols='50'
+                 autoComplete="off"
+                 onChange={(e) => setPhone(e.target.value)}/>
+             </div>
+             <button type="submit" className="send"><b>Send</b></button>
            </form>
         </div>
     )
