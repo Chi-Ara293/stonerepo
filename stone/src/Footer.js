c1bf251ecf4995dc316623388988d7c694806ad0
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-01-25 08:44:20 -0500

    add copyright icon

--- stone/src/Footer.js
+++ stone/src/Footer.js
@@ -1,7 +1,13 @@
+import copyright_icon from './assets/copyright_icon.png'
+
+const copyright = {
+    width:'10px',
+    height: 'auto',
+}
 const Footer = () => {
     return(
       <div className="footer">
-        <p>Copyright Grasstone 2025</p>
+        <p>Copyright <img src={copyright_icon} alt="copyright icon" style={copyright}/> Grasstone 2025</p>
       </div>
     )
 }
