5c2c6cda59497ce458a14e69bc009f35f023f63b
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-02-01 05:53:34 -0500

    small change

--- stone/src/About.js
+++ stone/src/About.js
@@ -3,9 +3,9 @@ const About = () => {
         <div className='about'>
           <h2 className='about-me'>About me</h2>
           <div className='p'>
-           <p>a freelance frontend developer</p>
-           <p>a poet</p>
-           <p>a part-time worker</p>
+           <p className='p1'>a freelance frontend developer</p>
+           <p className='p2'>a poet</p>
+           <p className='p3'>a part-time worker</p>
           </div>
         </div>
     )
