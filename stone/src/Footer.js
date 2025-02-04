94d6996add0645f230093e1d87b72af849036d91
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-02-03 19:20:37 -0500

    made change for copyright

--- stone/src/Footer.js
+++ stone/src/Footer.js
@@ -1,13 +1,7 @@
-import copyright_icon from './assets/copyright_icon.png'
-
-const copyright = {
-    width:'10px',
-    height: 'auto',
-}
 const Footer = () => {
     return(
       <div className="footer">
-        <p>Copyright <img src={copyright_icon} alt="copyright icon" style={copyright}/> Grasstone 2025</p>
+        <p>Copyright &copy; Grasstone 2025</p>
       </div>
     )
 }
