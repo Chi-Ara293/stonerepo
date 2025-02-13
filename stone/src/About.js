f55fa106877522ba8fdce90c6461c56bfcdcd004
Chigusa Araya <181899584+Chi-Ara293@users.noreply.github.com>
2025-02-13 07:07:27 -0500

    process

--- stone/src/About.js
+++ stone/src/About.js
@@ -1,14 +1,20 @@
+import chair from './assets/chair.jpg'
+
 const About = () => {
-    return(
-        <div className='about'>
-          <h2 className='about-me'>About me</h2>
-          <div className='p'>
-           <p className='p1'>a freelance frontend developer</p>
-           <p className='p2'>a poet</p>
-           <p className='p3'>a part-time worker</p>
-          </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className='about'>
+        <h2 className='about-me'>About me</h2>
+        <img className='aboutImage' src={chair} alt='chair drawing' />
+      </div>
+      <div>
+        <p className='p1'>&clubs; a freelance frontend developer<br />
+          (currently working on learning backend too)</p>
+        <p className='p2'>&hearts; a poet</p>
+        <p className='p3'>&diams; a part-time worker</p>
+      </div>
+    </div>
+  )
 }
 
 export default About;
